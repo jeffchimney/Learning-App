90453468796f49e6edd4430bfa8f6782bb5ee696
dnaismyth <naismythdayna@gmail.com>
2015-11-03 10:19:33 -0800

    Card Scripts

---
+++ CardFlipper.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardFlipper : MonoBehaviour {
+
+
+    SpriteRenderer spriteRenderer; // reference to card
+    CardModel model; // instance of CardModel class
+
+    public AnimationCurve scaleCurve; 
+    public float duration = 0.5f;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        model = GetComponent<CardModel>();
+        
+    }
+	
+    public void flipCard(Sprite startImage, Sprite endImage, int cardIndex)
+    {
+        StopCoroutine(Flip(startImage, endImage, cardIndex));
+        StartCoroutine(Flip(startImage, endImage, cardIndex));
+
+    }
+
+    //Method to flip the card
+    IEnumerator Flip(Sprite startImage, Sprite endImage, int cardIndex)
+    {
+        spriteRenderer.sprite = startImage;
+
+        float time = 0f;
+        while(time <= 1f)
+        {
+            float scale = scaleCurve.Evaluate(time);
+            time = time + Time.deltaTime / duration; // time taken to flip the card
+
+            Vector3 localScale = transform.localScale;
+            localScale.x = scale; // scaling the card on the x-axis (flat -> skinny -> flat)
+            transform.localScale = localScale;
+
+            if (time <= 0.5f)
+            {
+                spriteRenderer.sprite = endImage;
+            }
+
+            yield return new WaitForFixedUpdate(); 
+
+        }
+
+        if (cardIndex == -1)
+        {
+            model.ToggleFace(false); // show the back of the card
+        }
+        else
+        {
+            model.cardIndex = cardIndex;
+            model.ToggleFace(true); // show the face of the card
+        }
+    }
+}
---
+++ Deck.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Deck : MonoBehaviour
+{
+    List <int> cards;
+
+    //public method to iterate through deck of cards
+    public IEnumerable<int> GetCards()
+    {
+        foreach (int i in cards)
+        {
+            yield return i;
+        }
+    }
+
+    public void Shuffle()
+    {
+        if (cards == null)
+        {
+            cards = new List<int>();
+
+        }
+        else
+        {
+            cards.Clear(); // clear everything from the deck
+        }
+
+        for(int i = 0; i<52; i++)
+        {
+            cards.Add(i);
+        }
+
+        int cursor = cards.Count; //cursor through array
+        while(cursor > 1)
+        {
+            //Shuffle the deck randomly
+            cursor--;
+            int r = Random.Range(0, cursor + 1);
+            int temp = cards[r];
+            cards[r] = cards[cursor];
+            cards[cursor] = temp;
+            
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        Shuffle();
+    }
+
+    void Update() { }
+}
+	
