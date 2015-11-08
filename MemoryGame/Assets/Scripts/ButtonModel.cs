88e9e8388a2d58c8d85b2a191677c3209e2efcea
dnaismyth <naismythdayna@gmail.com>
2015-11-07 21:29:16 -0800

    Updating with shuffle method and button event listeners

--- MemoryGame/Assets/Scripts/ButtonModel.cs
+++ MemoryGame/Assets/Scripts/ButtonModel.cs
@@ -9,44 +9,69 @@ using System.Collections.Generic;
 public class ButtonModel : MonoBehaviour {
 
     SpriteRenderer spriteRenderer;
-    public Texture[] cardBacks;
-    public bool[] buttons;
+    public Texture faceDown;
+    public Texture[] cardBacks; //array to store picture cues
+    public Texture[] letters; // array to store the letters
+    public int cardIndex;
+    public CardFlipper flipper;
     
 
     /*Render the Sprites that will be provided into the cardBacks array*/
     void Awake()
     {
-        
+
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        flipper = GetComponent<CardFlipper>(); // create an instance of CardFlipper class
         
     }
 
-    /*Method to Shuffle the deck of cards*/
-    public void Shuffle(bool[] input)
+    // Method to Toggle the face of the card.  Once the game is starting, ToggleFace(false) to show backs of all input cards.
+    public void ToggleFace(bool showFace, int cardPressed)
     {
-        List<int> cards = new List<int>();
-        
 
-        for (int i = 0; i < input.Length; i++)
-        {
-            cards.Add(i); // add the cards into list to be shuffled
+       
+            if (showFace)
+            {
+                print("showing picture cue"); // change this for later, ex on click ToggleFace(true)
+            }
+            else
+            {
+                cardBacks[cardPressed] = faceDown;
+                letters[cardPressed] = faceDown;
+
         }
+    }
 
-        int n = cards.Count;
+
+
+    /*Method to Shuffle the deck of cards*/
+    public void Shuffle(Texture[] input1, Texture[] input2)
+    {
+        
+        
+        int n = input1.Length;
         //Begin shuffling list of input buttons
         while (n > 1)
         {
             n--;
             int k = Random.Range(0, n + 1);
-            int temp = cards[k];
-            cards[k] = cards[n];
-            cards[n] = temp;
+            int j = Random.Range(0, n + 1);
+            // Input picture cues and letter cues
+            Texture temp = input1[k];
+            input1[k] = input1[n];
+            input1[n] = temp;
+
+            Texture temp2 = input2[j];
+            input2[j] = input2[n];
+            input2[n] = temp2;
         }
+        
     }
 
     void OnGUI()
     {
-        
+        //TODO: Work on spacing for cards that will be displayed on bottom row, have them centered if columns < 6
         /******Spacing Variables**********/
         int buttonSizeWidth = 55;
         int buttonSizeHeight = 70;
@@ -55,12 +80,13 @@ public class ButtonModel : MonoBehaviour {
         int yOffset = 20;
         int numCols = 6;
         int numButtons = cardBacks.Length;
+        bool isShowing;
+       
         /**********************************/
-
-        buttons = new bool[numButtons]; // initalize button array to amount of cards chosen
         
         //Create parameters for button rectangle size
-        Rect r = new Rect(0, 0, buttonSizeWidth, buttonSizeHeight);
+        Rect r = new Rect(0, 0, buttonSizeWidth, buttonSizeHeight); // rect for picture cues
+        Rect r2 = new Rect(0, 0, buttonSizeWidth, buttonSizeHeight);
        
         //Loop through the amount of buttons user will have selected and assign a new Button object per texture.
         for(int i = 0; i < numButtons; i++)
@@ -70,13 +96,35 @@ public class ButtonModel : MonoBehaviour {
 
             Texture currTexture = cardBacks[i];
            
-            
-            buttons[i] = GUI.Button(new Rect(r), currTexture); //object type = bool (determine whether user has clicked (true) or not (false)
-            
+            // This needs work
+           if( GUI.Button(new Rect(r), currTexture) == true) //object type = bool (determine whether user has clicked (true) or not (false)
+            {
+                isShowing = false;
+                ToggleFace(false, i);
+            }
+          
 
         }
-        Shuffle(buttons); //call Shuffle method on buttons array
-        
-    }
+
+        for (int i = 0; i < numButtons; i++)
+        {
+
+            r2.x = xOffset + (i % numCols) * (buttonSizeWidth + buttonSpacing) + 500;
+            r2.y = yOffset + (int)Mathf.Floor((float)i / numCols) * (buttonSizeHeight + buttonSpacing);
+
+            Texture letterTexture = letters[i];
+
+            GUI.Button(new Rect(r2), letterTexture);
+
+        }
+
+
+
 
     }
+    void Start()
+    {
+        Shuffle(cardBacks, letters); // on start shuffle the given amount of buttons chosen
+    }
+
+}
---
+++ MemoryGame/Assets/Scripts/CardFlipper.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardFlipper : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    ButtonModel model;
+
+    public AnimationCurve scaleCurve; // create a parabolic shape curve in Unity Settings
+    public float duration = 0.5f; // time ease to flip the card
+
+    //Similar to Start(), show when the game is played
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        model = GetComponent<ButtonModel>();
+    }
+
+    //Method to flip the card, provide the startImage (cardback) and end Image (picture cue/letter cue) 
+    public void FlipCard(Texture startImage, Texture endImage, int cardIndex)
+    {
+        StopCoroutine(Flip(startImage, endImage, cardIndex));
+        StartCoroutine(Flip(startImage, endImage, cardIndex));
+    }
+    
+    /***************** This needs Work ************************/
+    //TODO: Figure out how to access and render texture image
+    // Might have to change to sprites
+    IEnumerator Flip(Texture startImage, Texture endImage, int cardIndex)
+    {
+        startImage = model.faceDown;
+        float time = 0f;
+        while (time <= 1f)
+        {
+            float scale = scaleCurve.Evaluate(time); //traces parabolic curve to determine the flipping motion of the card over the y-axis
+            time = time + Time.deltaTime / duration;
+
+            Vector3 localScale = transform.localScale;
+            localScale.x = scale;
+            transform.localScale = localScale;
+
+            if (time >= 0.5f)
+            {
+                startImage = model.cardBacks[cardIndex]; // once the time ease to flip the card is finished, show the face of the card (picutre, letter cues)
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        if (cardIndex == -1)
+        {
+            model.ToggleFace(false, cardIndex);
+        }
+        else
+        {
+            model.cardIndex = cardIndex;
+            model.ToggleFace(true, cardIndex);
+        }
+    }
+
+}
