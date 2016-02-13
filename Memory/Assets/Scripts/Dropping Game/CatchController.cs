f31be4814f26ee65f5340923b0372e80784dbdeb
dnaismyth <naismythdayna@gmail.com>
2016-02-12 18:24:37 -0800

    This will include start of a menu, all of the image assets for each letter so you won't have to download.  Includes dropping game, beginning string parsing with collected letters.

--- Memory/Assets/Scripts/Dropping Game/CatchController.cs
+++ Memory/Assets/Scripts/Dropping Game/CatchController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CatchController : MonoBehaviour {
 
@@ -10,9 +11,10 @@ public class CatchController : MonoBehaviour {
     private string collected = ""; // a string to hold the current letters that are collected
     private string imageName; // hold the name of the image we are trying to match the letters to 
     public ImageController currImage;
-  
-	// Update is called once per frame
-	void Update () {
+    private List<KeyValuePair<string, bool>> lettersCaught = new List<KeyValuePair<string, bool>>(); // hold the letter and bool to check if it has been printed
+
+    // Update is called once per frame
+    void Update () {
         setName();
 		// Decrement the x position when the left arrow is pressed down
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
@@ -38,11 +40,13 @@ public class CatchController : MonoBehaviour {
 
     // Destroy the coconut once it has collied with the catcher
     void OnCollisionEnter2D(Collision2D other) {
+
 		if (other.gameObject.tag == "Coconut") {
 			//pos = pos + 15;
 			currentLetter = other.gameObject.GetComponent<SpriteRenderer> ().sprite.name.ToString ();
             if (imageName.Contains(currentLetter)) // check if the current letter being caught matches a letter in the current image being displayed
             {
+                lettersCaught.Add(new KeyValuePair<string, bool>(currentLetter, false));
                 collected = collected + currentLetter;
                 Debug.Log(collected.ToString());
             }
@@ -62,11 +66,27 @@ public class CatchController : MonoBehaviour {
 
 		
 	void OnGUI () {
-		GUI.contentColor = Color.black;
+		GUI.contentColor = Color.clear;
 		style.fontSize = 33;
-		GUI.Label (new Rect (pos,Screen.height/2,100,50), getCollected(), style);
-	}
-	
+        char[] imgChars = imageName.ToCharArray();  // split the image name into individual characters to check for matching
+        char[] collChars = new char[imageName.Length]; // split the collected letters into individual characters
+        collChars = collected.ToCharArray();    // change this to equal our key value pairs
+ 
+        //Debug.Log()
+        for (int i = 0; i<collChars.Length; i++)
+        {
+            if (imageName.Contains(collChars[i].ToString()))
+            {
+                int index = imageName.IndexOf(collChars[i]);
+                GUI.contentColor = Color.black;
+                GUI.Label(new Rect((pos*index)/3, Screen.height / 2, 100, 50), collChars[i].ToString(), style); // print the letter to screen based on it's position in the word
+
+            }
+        }
+
+
+    }
+
 
 
 
