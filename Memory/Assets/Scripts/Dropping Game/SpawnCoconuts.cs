a3a9269ddd769cee1e104996e59df0d169416be8
Jeff Chimney <jeffchimney@icloud.com>
2016-03-20 16:49:49 -0700

    Adding win screen assets

--- Memory/Assets/Scripts/Dropping Game/SpawnCoconuts.cs
+++ Memory/Assets/Scripts/Dropping Game/SpawnCoconuts.cs
@@ -5,7 +5,6 @@ public class SpawnCoconuts : MonoBehaviour {
 
 	private int id; // give each letter an ID so we can refer to it
 	private AudioClip clip;
-    private string let;
 
 
 
@@ -19,15 +18,9 @@ public class SpawnCoconuts : MonoBehaviour {
         return this.clip;
     }
 
-    public string getLetter()
-    {
-        return let;
-    }
-
 	// Set a coconut to the input array of sprite images (coconuts with different letters on each)
 	// Will have to change to add more parameters later (ex: sound of letters, etc)
-	public SpawnCoconuts setCoconut(Sprite image, AudioClip c, int inputId, string letter){
-        let = letter;
+	public SpawnCoconuts setCoconut(Sprite image, AudioClip c, int inputId){
 		id = inputId;
         clip = c;
 		GetComponent<SpriteRenderer>().sprite = image;
---
+++ Memory/Assets/Scripts/Dropping Game/WinOverlay.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinOverlay : MonoBehaviour 
+{
+	protected bool isShowing = false;
+	public GameObject menu; // Assign in inspector
+	public AudioClip congratulations;
+	AudioSource playAudio;
+
+	public void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			isShowing = !isShowing;
+		}
+
+		if(isShowing)
+		{
+			if(!GetComponent<ParticleSystem>().isPlaying)
+			{
+				GetComponent<ParticleSystem>().Play();
+				menu.SetActive(isShowing);
+				playAudio.PlayOneShot(congratulations);
+			}
+		}else{
+			if(GetComponent<ParticleSystem>().isPlaying)
+			{
+				GetComponent<ParticleSystem>().Stop();
+				menu.SetActive(isShowing);
+			}
+		}
+	}
+}
